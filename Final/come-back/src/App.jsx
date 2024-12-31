5c2db1f494d98dd4c79ffea0c1ee095820a4bb5e
NAVEEN KUMAR <nk946432@gmail.com>
2024-12-31 20:51:40 +0530

    learned all stuffs about the useState

--- Final/come-back/src/App.jsx
+++ Final/come-back/src/App.jsx
@@ -1,20 +1,115 @@
-import React from 'react'
-import Header from './Components/Header'
-import Main from './Components/Main'
-import Footer from './Components/Footer'
-import Weather from "./Components/Weather"
-import UserStatus from './Components/UserStatus'
+import React, { useState } from "react";
+import UseState from "./Components/UseState";
 
 const App = () => {
+
+  //normal usestate
+  
+  const [count, setCount] = useState(0);
+  console.log(count);
+  console.log(setCount);
+
+  const increment = () => {
+    return setCount(count + 1);
+  };
+  const decrement = () => {
+    return setCount(count - 1);
+  };
+
+  //Array useState
+
+  const [friends, setFriends] = useState(["Alex", "john"]);
+
+  const addNewFriend = () => {
+    return setFriends([...friends, "naveen"]);
+  };
+
+  const removeFriend = () => {
+    return setFriends(friends.filter((f) => f !== "john"));
+  };
+
+  const updateOnefriend = () => {
+    return setFriends(friends.map((f) => (f === "Alex" ? "Alex Smith" : f)));
+  };
+
+  //object useState
+
+  const [movie, setMovie] = useState({
+    title: "RRR",
+    rating: 5,
+  });
+
+  const handleRating = () => {
+    setMovie({ ...movie, rating: 9 });
+  };
+
+  //Array with object useState
+
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "spiderman",
+      rating: 8,
+    },
+    {
+      id: 2,
+      title: "AntMan",
+      rating: 8,
+    },
+    {
+      id: 3,
+      title: "superMan",
+      rating: 8,
+    },
+  ]);
+
+  const movieChange = () => {
+    setMovies(
+      movies.map((m) => (m.id === 1 ? {...movies, title: "naveenMan"   } : m))
+
+    );
+  };
+
   return (
     <div>
-      <Header/>
-      <Main/>
-      <Footer />
-      <UserStatus loggedIn={true} isAdmin={true}/>
+      <section>
+        <h1>{count}</h1>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
 
+        <p>
+          {friends.map((names) => (
+            <li key={Math.random()}>{names}</li>
+          ))}
+        </p>
+
+        <button onClick={addNewFriend}>Add a New Friend</button>
+        <button onClick={removeFriend}>remove Friend</button>
+        <button onClick={updateOnefriend}>update one frined</button>
+
+        <h1>{movie.title}</h1>
+        <p>{movie.rating}</p>
+
+        <button onClick={handleRating}>increase movie rating</button>
+
+        <br />
+        <br />
+
+        <p>
+          {movies.map((move) => (
+            <ul key={move.id}>
+              <li>{move.title}</li>
+              <li>{move.rating}</li>
+            </ul>
+          ))}
+        </p>
+
+        <button onClick={movieChange}>Change Name</button>
+      </section>
+
+      <UseState/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Final/come-back/src/main.jsx
+++ Final/come-back/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import UseState from './Components/UseState.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
   </StrictMode>,
 )
